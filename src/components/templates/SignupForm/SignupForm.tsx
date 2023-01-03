2d58cb388e47986a01a018974727fd4b4dc4c799
mini <0miiii@naver.com>
2023-01-04 03:47:18 +0900

    ✨ Feat: input 핸들러, 유효성 검사

---
+++ src/components/templates/SignupForm/SignupForm.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { SIGNUP } from "../../../constants/path";
+import Button from "../../atoms/Button/Button";
+
+const SignupForm = () => {
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPW, setInputPW] = useState("");
+  const [inputMatchPW, setInputMatchPW] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPW, setValidPW] = useState(false);
+  const [matchPW, setMatchPW] = useState(false);
+
+  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEmail(event.target.value);
+  };
+
+  const pwChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPW(event.target.value);
+  };
+
+  const matchPwChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputMatchPW(event.target.value);
+  };
+
+  const validateEmailHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    if (isEmail(input)) {
+      return setValidEmail(true);
+    }
+    return setValidEmail(false);
+  };
+
+  const validatePwHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    if (isMoreThan8Length(input)) {
+      return setValidPW(true);
+    }
+    return setValidPW(false);
+  };
+
+  const validateMatchPwHandler = (
+    event: React.FocusEvent<HTMLInputElement>
+  ) => {
+    const input = event.target.value;
+    if (doPasswordMatch(inputPW, input)) {
+      return setMatchPW(true);
+    }
+    return setMatchPW(false);
+  };
+
+  const isEmail = (email: string) => {
+    const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    if (regex.test(email)) {
+      return true;
+    }
+    return false;
+  };
+
+  const isMoreThan8Length = (pw: string) => {
+    if (pw.length >= 8) {
+      return true;
+    }
+    return false;
+  };
+
+  const doPasswordMatch = (pw: string, checkPw: string) => {
+    if (pw === checkPw) {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="email">email</label>
+        <input
+          id="email"
+          type="text"
+          placeholder={SIGNUP.EMAIL}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <div>
+          {validEmail ? SIGNUP.EMAIL_VALID_SUCCESS : SIGNUP.EMAIL_VALID_FAIL}
+        </div>
+
+        <label htmlFor="pw">password</label>
+        <input
+          id="pw"
+          type="text"
+          placeholder={SIGNUP.PW}
+          onChange={pwChangeHandler}
+          onBlur={validatePwHandler}
+        />
+        <div>{validPW ? SIGNUP.PW_VALID_SUCCESS : SIGNUP.PW_VALID_FAIL}</div>
+
+        <label htmlFor="match-pw">password check</label>
+        <input
+          id="match-pw"
+          type="text"
+          placeholder={SIGNUP.PW}
+          onChange={matchPwChangeHandler}
+          onBlur={validateMatchPwHandler}
+        />
+        <div>{matchPW ? SIGNUP.PW_MATCH_SUCCESS : SIGNUP.PW_MATCH_FAIL}</div>
+        <Button>가입하기</Button>
+      </form>
+    </>
+  );
+};
+
+export default SignupForm;
