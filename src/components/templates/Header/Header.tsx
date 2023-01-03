f7e39f4e1f1f1e362826a860339e09581a0074b2
mini <0miiii@naver.com>
2023-01-04 02:41:46 +0900

    ✨ Feat: 로그인, 회원가입 버튼 추가

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from "react"
+import Button from "../../atoms/Button/Button"
+import { Link } from "react-router-dom"
+import { PATH } from "../../../constants/path"
+import Container from "./Header.styles"
 
 const Header = () => {
-  return <div>Header</div>;
-};
+  return (
+    <Container>
+      <Link to={PATH.LOGIN}>
+        <Button>Login</Button>
+      </Link>
+      <Link to={PATH.SIGNUP}>
+        <Button>SignUp</Button>
+      </Link>
+    </Container>
+  )
+}
 
-export default Header;
+export default Header
