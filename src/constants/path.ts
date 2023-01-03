12fc5979c9ffda6cc96c0ad964618f255e700038
mini <0miiii@naver.com>
2023-01-04 02:39:10 +0900

    ✨ Feat: path Login Singup 추가

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,7 +1,21 @@
-const PATH = {
+export const PATH = {
   MAIN: "/",
   LOGIN: "login",
   SIGNUP: "signup",
-};
+}
 
-export default PATH;
+export const LOGIN = {
+  EMAIL: "이메일을 입력하세요",
+  PW: "비밀번호를 입력하세요",
+}
+
+export const SIGNUP = {
+  EMAIL: "이메일을 입력하세요",
+  EMAIL_VALID_SUCCESS: "사용가능한 이메일입니다",
+  EMAIL_VALID_FAIL: "이메일이 적절하지 않습니다",
+  PW: "비밀번호를 입력하세요",
+  PW_VALID_SUCCESS: "사용가능한 비밀번호입니다.",
+  PW_VALID_FAIL: "비밀번호를 8자 이상 입력해주세요",
+  PW_CHECK_SUCCESS: "비밀번호가 일치합니다",
+  PW_CHECK_FAIL: "비밀번호가 일치하지 않습니다",
+}
