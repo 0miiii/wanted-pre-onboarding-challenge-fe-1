ea72629d6c11d59eeca9ac39757dd8cc49ecc003
mini <0miiii@naver.com>
2023-01-04 03:44:51 +0900

    ✨ Feat: SIGNUP 상수 변경

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,12 +2,12 @@ export const PATH = {
   MAIN: "/",
   LOGIN: "login",
   SIGNUP: "signup",
-}
+};
 
 export const LOGIN = {
   EMAIL: "이메일을 입력하세요",
   PW: "비밀번호를 입력하세요",
-}
+};
 
 export const SIGNUP = {
   EMAIL: "이메일을 입력하세요",
@@ -16,6 +16,6 @@ export const SIGNUP = {
   PW: "비밀번호를 입력하세요",
   PW_VALID_SUCCESS: "사용가능한 비밀번호입니다.",
   PW_VALID_FAIL: "비밀번호를 8자 이상 입력해주세요",
-  PW_CHECK_SUCCESS: "비밀번호가 일치합니다",
-  PW_CHECK_FAIL: "비밀번호가 일치하지 않습니다",
-}
+  PW_MATCH_SUCCESS: "비밀번호가 일치합니다",
+  PW_MATCH_FAIL: "비밀번호가 일치하지 않습니다",
+};
