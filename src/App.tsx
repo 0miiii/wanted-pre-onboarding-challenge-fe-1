9119909e8b21b32741eab1fc06378610c29709e6
mini <0miiii@naver.com>
2023-01-05 02:04:25 +0900

    ✨ Feat: 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import Login from "./pages/Login/Login"
-import Signup from "./pages/Signup/Signup"
-import Main from "./pages/Main/Main"
-import Header from "./components/templates/Header/Header"
-import { PATH } from "./constants/path"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth/Auth";
+import Main from "./pages/Main/Main";
+import LoginForm from "./components/templates/LoginForm/LoginForm";
+import SignupForm from "./components/templates/SignupForm/SignupForm";
+import { PATH } from "./constants/path";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path={PATH.LOGIN} element={<Login />} />
-        <Route path={PATH.SIGNUP} element={<Signup />} />
+        <Route path={PATH.AUTH} element={<Auth />}>
+          <Route path={PATH.LOGIN} element={<LoginForm />} />
+          <Route path={PATH.SIGNUP} element={<SignupForm />} />
+        </Route>
         <Route path={PATH.MAIN} element={<Main />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
