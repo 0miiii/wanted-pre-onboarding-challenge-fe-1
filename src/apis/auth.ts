12ec089a18852604573f5e1a7bb32c2b9cf40242
mini <0miiii@naver.com>
2023-01-05 03:35:41 +0900

    ✨ Feat: 로그인, 회원가입이 둘다 가능하도록 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { userInfoType } from "../types/user";
 
-export const signup_request = async (userInfo: userInfoType) => {
+export const auth_request = async (auth: string, userInfo: userInfoType) => {
+  let endpoint = auth === "signup" ? "create" : "login";
+
   try {
     const { data } = await axios.post(
-      "http://localhost:8080/users/create",
+      `http://localhost:8080/users/${endpoint}`,
       userInfo
     );
     return data;
