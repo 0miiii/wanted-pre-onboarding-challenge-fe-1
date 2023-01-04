cfb44b72e5505d9610c2acaed19c854afa5b5fe4
mini <0miiii@naver.com>
2023-01-04 20:58:26 +0900

    🎨 Styling: path와 auth 분리

---
+++ src/constants/auth.ts
@@ -0,0 +1,15 @@
+export const LOGIN = {
+  EMAIL: "이메일을 입력하세요",
+  PW: "비밀번호를 입력하세요",
+};
+
+export const SIGNUP = {
+  EMAIL: "이메일을 입력하세요",
+  EMAIL_VALID_SUCCESS: "사용가능한 이메일입니다",
+  EMAIL_VALID_FAIL: "이메일이 적절하지 않습니다",
+  PW: "비밀번호를 입력하세요",
+  PW_VALID_SUCCESS: "사용가능한 비밀번호입니다.",
+  PW_VALID_FAIL: "비밀번호를 8자 이상 입력해주세요",
+  PW_MATCH_SUCCESS: "비밀번호가 일치합니다",
+  PW_MATCH_FAIL: "비밀번호가 일치하지 않습니다",
+};
