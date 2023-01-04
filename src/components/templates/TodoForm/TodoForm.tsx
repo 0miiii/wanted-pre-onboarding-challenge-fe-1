8ec1ff15ba4cb9ffea242fee5d543c71d2c12eae
mini <0miiii@naver.com>
2023-01-05 04:31:19 +0900

    ✨ Feat: 인풋, 서브밋 핸들러 생성

--- src/components/templates/TodoForm/TodoForm.tsx
+++ src/components/templates/TodoForm/TodoForm.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../atoms/Button/Button";
 
 const TodoForm = () => {
-  return <div>TodoForm</div>;
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredContent, setEnteredContent] = useState("");
+
+  const titleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const contentChangeHandler = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setEnteredContent(event.target.value);
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (enteredTitle === "" && enteredContent === "") {
+      alert("글을 모두 입력해주세요");
+      return;
+    }
+
+    setEnteredTitle("");
+    setEnteredContent("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="title">title</label>
+      <input
+        id="title"
+        type="text"
+        onChange={titleChangeHandler}
+        value={enteredTitle}
+      />
+      <label htmlFor="content">content</label>
+      <textarea
+        id="content"
+        onChange={contentChangeHandler}
+        value={enteredContent}
+      />
+      <Button>작성완료</Button>
+    </form>
+  );
 };
 
 export default TodoForm;
