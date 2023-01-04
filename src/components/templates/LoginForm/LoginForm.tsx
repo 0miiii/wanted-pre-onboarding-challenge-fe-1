56245836b52a5b2a12d4baea3c14181cf69b44d2
mini <0miiii@naver.com>
2023-01-05 03:36:25 +0900

    ✨ Feat: 로그인 로직 생성

--- src/components/templates/LoginForm/LoginForm.tsx
+++ src/components/templates/LoginForm/LoginForm.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import { LOGIN } from "../../../constants/auth";
 import Button from "../../atoms/Button/Button";
+import { auth_request } from "../../../apis/auth";
 
 const LoginForm = () => {
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPW, setInputPW] = useState("");
+  const navigate = useNavigate();
+
+  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputPW(event.target.value);
+  };
+
+  const loginHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputEmail === "" && inputPW === "") {
+      alert("이메일과 비밀번호를 입력해주세요");
+      return;
+    }
+    const userInfo = { email: inputEmail, password: inputPW };
+    const response = await auth_request("login", userInfo);
+    alert(response.message);
+    localStorage.setItem("token", response.token);
+    navigate("/main");
+    return;
+  };
+
   return (
-    <main>
-      <input type="text" placeholder={LOGIN.EMAIL} />
-      <input type="text" placeholder={LOGIN.PW} />
+    <form onSubmit={loginHandler}>
+      <input
+        type="text"
+        placeholder={LOGIN.EMAIL}
+        onChange={emailChangeHandler}
+      />
+      <input
+        type="text"
+        placeholder={LOGIN.PW}
+        onChange={passwordChangeHandler}
+      />
       <Button>로그인</Button>
-      <Button>회원가입</Button>
-    </main>
+    </form>
   );
 };
 
