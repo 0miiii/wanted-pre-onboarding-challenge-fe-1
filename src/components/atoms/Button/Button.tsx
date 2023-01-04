95ac85684d750175c564de6a70e46770214efbcf
mini <0miiii@naver.com>
2023-01-05 03:06:11 +0900

    ✨ Feat: onClick props 추가

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,10 +3,15 @@ import React from "react";
 interface Props {
   children: React.ReactNode;
   disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<Props> = ({ children, disabled }) => {
-  return <button disabled={disabled}>{children}</button>;
+const Button: React.FC<Props> = ({ children, disabled, onClick }) => {
+  return (
+    <button disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
