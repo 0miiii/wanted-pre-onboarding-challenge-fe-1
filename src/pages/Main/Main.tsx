b3cf7e3258deb24b90832406055fb1c12a7bb5e8
yeongmin <0miiii@naver.com>
2023-01-05 18:39:00 +0900

    feat: 투두 요청 후 props 전달

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
+import { getTodos_request } from "../../apis/todo";
 import Button from "../../components/atoms/Button/Button";
 import TodoForm from "../../components/templates/TodoForm/TodoForm";
+import TodoList from "../../components/templates/TodoList/TodoList";
+
+type getTodoType = {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
 const Main = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
+  const [todoList, setTodoList] = useState<getTodoType[]>([]);
+
+  useEffect(() => {
+    getTodos_request(token).then((res) => {
+      const { data } = res;
+      setTodoList(data);
+    });
+  }, [token]);
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
@@ -18,7 +36,8 @@ const Main = () => {
       {token ? (
         <>
           <Button onClick={logoutHandler}>로그아웃</Button>
-          <TodoForm />
+          <TodoList todoList={todoList} />
+          <TodoForm addTodoList={setTodoList} />
         </>
       ) : (
         <div>잘못된 접근입니다.</div>
