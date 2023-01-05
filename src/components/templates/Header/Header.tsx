6a0413fff255332f1c7c90ad6c9ab778faf45910
yeongmin <0miiii@naver.com>
2023-01-05 19:52:22 +0900

    feat: 로그아웃 핸들러 생성

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../atoms/Button/Button";
 import { Link } from "react-router-dom";
 import { PATH } from "../../../constants/path";
 import Container from "./Header.styles";
 
-const Header = () => {
+const Header: React.FC<{
+  token: string | null;
+  deleteToken: React.Dispatch<React.SetStateAction<string | null>>;
+}> = ({ token, deleteToken }) => {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    deleteToken(localStorage.getItem("token"));
+    alert("로그아웃 성공");
+    navigate("/login");
+  };
+
   return (
     <Container>
-      <Link to={PATH.LOGIN}>
-        <Button>Login</Button>
-      </Link>
-      <Link to={PATH.SIGNUP}>
-        <Button>SignUp</Button>
-      </Link>
+      {!token ? (
+        <>
+          <Link to={PATH.LOGIN}>
+            <Button>Login</Button>
+          </Link>
+          <Link to={PATH.SIGNUP}>
+            <Button>SignUp</Button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Button onClick={logoutHandler}>Logout</Button>
+        </>
+      )}
     </Container>
   );
 };
