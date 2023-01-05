a7a03c71fb5544976e9ebf65c644dc3ea4ba7837
yeongmin <0miiii@naver.com>
2023-01-06 00:16:58 +0900

    feat: todo 상세정보 요청 후 렌더링

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getTodosById_request } from "../../apis/todo";
+import { Todo } from "../../types/todo";
+import Button from "../../components/atoms/Button/Button";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const [todo, setTodo] = useState<Todo>();
+  const { id } = useParams();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    getTodosById_request(id, token).then((res) => {
+      const { data } = res;
+      setTodo(data);
+    });
+  }, [id, token]);
+
+  return (
+    <div>
+      <h1>title</h1>
+      <div>{todo?.title}</div>
+      <h2>content</h2>
+      <div>{todo?.content}</div>
+      <Button>수정하기</Button>
+      <Button>삭제하기</Button>
+    </div>
+  );
 };
 
 export default Detail;
