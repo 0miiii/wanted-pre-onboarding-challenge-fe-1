ca49d9f6e174849603b328280d8c2d566f801736
yeongmin <0miiii@naver.com>
2023-01-05 19:52:41 +0900

    feat: token 관리 및 props 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Auth from "./pages/Auth/Auth";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Header from "./components/templates/Header/Header";
 import Main from "./pages/Main/Main";
-import LoginForm from "./components/templates/LoginForm/LoginForm";
-import SignupForm from "./components/templates/SignupForm/SignupForm";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 import { PATH } from "./constants/path";
 
 function App() {
+  const [token, setToken] = useState<string | null>("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getToken = localStorage.getItem("token");
+    setToken(getToken);
+  }, []);
+
+  if (token) {
+    navigate("/main");
+  }
+
   return (
     <>
+      <Header token={token} deleteToken={setToken} />
       <Routes>
-        <Route path={PATH.AUTH} element={<Auth />}>
-          <Route path={PATH.LOGIN} element={<LoginForm />} />
-          <Route path={PATH.SIGNUP} element={<SignupForm />} />
-        </Route>
+        <Route path={PATH.LOGIN} element={<Login addToken={setToken} />} />
+        <Route path={PATH.SIGNUP} element={<SignUp addToken={setToken} />} />
         <Route path={PATH.MAIN} element={<Main />} />
       </Routes>
     </>
