4650897c1f0a7351a55361617be013bced857843
mini <0miiii@naver.com>
2023-01-18 05:13:49 +0900

    refactor: change api and change state to ref

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,53 +1,71 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
+import { AxiosResponse } from "axios";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
+import { loginHandler } from "../../store/reducers/authSlice";
 import { LOGIN } from "../../constants/auth";
+import instance from "../../apis/instance";
 import Button from "../../components/atoms/Button/Button";
-import { auth_request } from "../../apis/auth";
 
-const Login: React.FC<{
-  addToken: React.Dispatch<React.SetStateAction<string | null>>;
-}> = ({ addToken }) => {
-  const [inputEmail, setInputEmail] = useState("");
-  const [inputPW, setInputPW] = useState("");
+type LoginResponse = { token: string; message: string };
+
+const InputContainer = styled.div`
+  background-color: antiquewhite;
+  border-radius: 5px;
+  width: 70%;
+  padding: 10px;
+  margin: 10px;
+`;
+
+const Login = () => {
+  const enteredEmail = useRef<HTMLInputElement>(null);
+  const enteredPassword = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputEmail(event.target.value);
-  };
-
-  const passwordChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setInputPW(event.target.value);
-  };
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (inputEmail === "" && inputPW === "") {
-      alert("이메일과 비밀번호를 입력해주세요");
-      return;
+    const email = enteredEmail.current?.value;
+    const password = enteredPassword.current?.value;
+
+    if (email === "" && password === "") {
+      return alert("이메일과 비밀번호를 입력해주세요");
     }
-    const userInfo = { email: inputEmail, password: inputPW };
-    const response = await auth_request("login", userInfo);
-    alert(response.message);
-    localStorage.setItem("token", response.token);
-    addToken(response.token);
-    navigate("/main");
-    return;
+
+    try {
+      const userInfo = { email, password };
+      const response: AxiosResponse<LoginResponse> = await instance.post(
+        "/users/login",
+        userInfo
+      );
+      dispatch(loginHandler(response.data.token));
+      navigate("/main");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="email"
-        placeholder={LOGIN.EMAIL}
-        onChange={emailChangeHandler}
-      />
-      <input
-        type="password"
-        placeholder={LOGIN.PW}
-        onChange={passwordChangeHandler}
-      />
+      <InputContainer>
+        <label htmlFor="email">email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder={LOGIN.EMAIL}
+          ref={enteredEmail}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label htmlFor="password">password</label>
+        <input
+          id="password"
+          type="password"
+          placeholder={LOGIN.PW}
+          ref={enteredPassword}
+        />
+      </InputContainer>
       <Button>로그인</Button>
     </form>
   );
