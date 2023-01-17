523b4e60c163bf540508728974306133e9863d72
mini <0miiii@naver.com>
2023-01-17 23:25:29 +0900

    feat: manage token in redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/templates/Header/Header";
 import Main from "./pages/Main/Main";
@@ -9,19 +9,12 @@ import SignUp from "./pages/SignUp/SignUp";
 import { PATH } from "./constants/path";
 
 const App = () => {
-  const [token, setToken] = useState<string | null>("");
-
-  useEffect(() => {
-    const getToken = localStorage.getItem("token");
-    setToken(getToken);
-  }, []);
-
   return (
     <>
-      <Header token={token} deleteToken={setToken} />
+      <Header />
       <Routes>
-        <Route path={PATH.LOGIN} element={<Login addToken={setToken} />} />
-        <Route path={PATH.SIGNUP} element={<SignUp addToken={setToken} />} />
+        <Route path={PATH.LOGIN} element={<Login />} />
+        <Route path={PATH.SIGNUP} element={<SignUp />} />
         <Route path={PATH.MAIN} element={<Main />} />
         <Route path={PATH.EDIT} element={<Edit />} />
         <Route path={PATH.DETAIL} element={<Detail />} />
