eaba929b5202e0df156321c4bddbe51ece3337aa
mini <0miiii@naver.com>
2023-01-17 14:44:45 +0900

    refactor: remove unnecessary if statements

--- src/utils/validCheck.ts
+++ src/utils/validCheck.ts
@@ -1,23 +1,13 @@
 export const isEmail = (email: string) => {
   const emailRegExp = "[a-z0-9]+@[a-z]+.[a-z]{2,3}";
   const regex = new RegExp(emailRegExp);
-
-  if (regex.test(email)) {
-    return true;
-  }
-  return false;
+  return regex.test(email);
 };
 
 export const isMoreThan8Length = (pw: string) => {
-  if (pw.length >= 8) {
-    return true;
-  }
-  return false;
+  return pw.length >= 8;
 };
 
 export const doMatch = (a: string, b: string) => {
-  if (a === b) {
-    return true;
-  }
-  return false;
+  return a === b;
 };
