7956ffe41709f0d6f3f30b98a2e408c375556148
yeongmin <0miiii@naver.com>
2023-01-18 17:10:59 +0900

    refactor: reset routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/templates/Header/Header";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
 import Main from "./pages/Main/Main";
-import Edit from "./pages/Edit/Edit";
-import Detail from "./pages/Detail/Detail";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import PATH from "./constants/path";
+import GlobalLayout from "./layouts/GlobalLayout/GlobalLayout";
 
 const App = () => {
+  const isLogin = useSelector((state: RootState) => state.auth.isLoggedIn);
+
   return (
-    <>
-      <Header />
+    <GlobalLayout>
       <Routes>
         <Route path={PATH.LOGIN} element={<Login />} />
         <Route path={PATH.SIGNUP} element={<SignUp />} />
-        <Route path={PATH.MAIN} element={<Main />} />
-        <Route path={PATH.EDIT} element={<Edit />} />
-        <Route path={PATH.DETAIL} element={<Detail />} />
+        <Route path={PATH.MAIN} element={isLogin && <Main />} />
         <Route path="*" element={<div>잘못된 접근입니다.</div>} />
       </Routes>
-    </>
+    </GlobalLayout>
   );
 };
 
