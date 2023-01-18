c6eebdd5b73bc386ba1c5aa2a60841a9c6a16e80
yeongmin <0miiii@naver.com>
2023-01-18 17:24:27 +0900

    refactor: change structure

--- src/components/templates/TodoList/TodoList.tsx
+++ src/components/templates/TodoList/TodoList.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Todo } from "../../../types/todo";
+import Container from "./TodoList.style";
+import TodoForm from "../../molecules/TodoForm/TodoForm";
 
-const TodoList: React.FC<{ todoList: Todo[] }> = ({ todoList }) => {
+type Props = {
+  todoList: Todo[];
+  onAddTodo: React.Dispatch<React.SetStateAction<Todo[]>>;
+  onClickedTodo: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const TodoList: React.FC<Props> = ({ todoList, onAddTodo, onClickedTodo }) => {
+  const clickHandler = (id: string) => {
+    onClickedTodo(id);
+  };
+
   return (
-    <ul>
-      {todoList.map((todo) => (
-        <Link to={`/detail/${todo.id}`} key={todo.id}>
-          <li>{todo.title}</li>
-        </Link>
-      ))}
-    </ul>
+    <Container>
+      <TodoForm onAddTodo={onAddTodo} />
+      <ul>
+        {todoList.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => clickHandler(todo.id)}
+            aria-hidden="true"
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
 };
 
