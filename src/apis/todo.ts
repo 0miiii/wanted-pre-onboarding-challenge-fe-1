32c04b3f6367512d45c42a77ebdd77b417824542
mini <0miiii@naver.com>
2023-01-19 22:37:38 +0900

    fix: type fix

--- src/apis/todo.ts
+++ src/apis/todo.ts
@@ -3,16 +3,16 @@ import { RequestTodo, Todo } from "../types/todo";
 
 const todoApi = {
   getTodos: () => {
-    return instance.get<Todo[]>("/todos");
+    return instance.get<{ data: Todo[] }>("/todos");
   },
   getTodosById: (id: string) => {
-    return instance.get<Todo>(`/todos/${id}`);
+    return instance.get<{ data: Todo }>(`/todos/${id}`);
   },
   createTodo: (todo: RequestTodo) => {
-    return instance.post<Todo>("/todos", todo);
+    return instance.post<{ data: Todo }>("/todos", todo);
   },
   updateTodo: (id: string, todo: RequestTodo) => {
-    return instance.put<Todo>(`todos/${id}`, todo);
+    return instance.put<{ data: Todo }>(`todos/${id}`, todo);
   },
   deleteTodo: (id: string) => {
     return instance.delete<null>(`/todos/${id}`);
