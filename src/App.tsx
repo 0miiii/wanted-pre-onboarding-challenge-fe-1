53c2016f703be1027c928b43583462285538dbc7
mini <0miiii@naver.com>
2023-01-20 02:41:35 +0900

    feat: move path when not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import { RootState } from "./store/store";
 import Main from "./pages/Main/Main";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import Error from "./pages/Error/Error";
 import PATH from "./constants/path";
 import GlobalLayout from "./layouts/GlobalLayout/GlobalLayout";
 
 const App = () => {
   const isLogin = useSelector((state: RootState) => state.auth.isLoggedIn);
-
   return (
     <GlobalLayout>
       <Routes>
-        <Route path={PATH.LOGIN} element={!isLogin && <Login />} />
+        <Route path={PATH.LOGIN} element={!isLogin ? <Login /> : <Error />} />
         <Route path={PATH.SIGNUP} element={<SignUp />} />
-        <Route path={PATH.MAIN} element={isLogin && <Main />} />
-        <Route path="*" element={<div>잘못된 접근입니다.</div>} />
+        <Route path={PATH.MAIN} element={isLogin ? <Main /> : <Error />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </GlobalLayout>
   );
