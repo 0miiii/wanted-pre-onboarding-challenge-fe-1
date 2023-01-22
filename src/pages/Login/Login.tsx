634712a8bb58f6613d6ee63f8108d2a24579716c
mini <0miiii@naver.com>
2023-01-23 00:34:31 +0900

    feat: Component segmentation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,63 +1,8 @@
-import React, { useRef } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
-import { tokenSaveHandler } from "../../store/reducers/authSlice";
-import { LOGIN } from "../../constants/auth";
-import authApi from "../../apis/auth";
-import { FormContainer, InputContainer } from "./Login.style";
-import Button from "../../components/atoms/Button/Button";
-import { EnteredUserInfo } from "../../types/user";
+import React from "react";
+import LoginForm from "../../components/templates/LoginForm/LoginForm";
 
 const Login = () => {
-  const enteredEmail = useRef<HTMLInputElement>(null);
-  const enteredPassword = useRef<HTMLInputElement>(null);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const loginHandler = async (userInfo: EnteredUserInfo) => {
-    const response = await authApi.login(userInfo);
-    dispatch(tokenSaveHandler(response.data.token));
-    alert(response.data.message);
-    navigate("/main");
-  };
-
-  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const email = enteredEmail.current?.value;
-    const password = enteredPassword.current?.value;
-    const userInfo = { email, password };
-
-    if (email === "" && password === "") {
-      alert("이메일과 비밀번호를 입력해주세요");
-      return;
-    }
-
-    loginHandler(userInfo).catch((err) => alert(err));
-  };
-
-  return (
-    <FormContainer onSubmit={submitHandler}>
-      <InputContainer>
-        <label htmlFor="email">email</label>
-        <input
-          id="email"
-          type="email"
-          placeholder={LOGIN.EMAIL}
-          ref={enteredEmail}
-        />
-      </InputContainer>
-      <InputContainer>
-        <label htmlFor="password">password</label>
-        <input
-          id="password"
-          type="password"
-          placeholder={LOGIN.PW}
-          ref={enteredPassword}
-        />
-      </InputContainer>
-      <Button>로그인</Button>
-    </FormContainer>
-  );
+  return <LoginForm />;
 };
 
 export default Login;
